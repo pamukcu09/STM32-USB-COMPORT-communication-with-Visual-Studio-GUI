30af03e67c2e1ac03cec94bac9d54638d4aaddae
Mustafa Pamukçu <64636233+pamukcu09@users.noreply.github.com>
2022-04-19 17:51:48 +0300

    stm32f103c8t6

---
+++ STM32 GUI/Arduino GUI/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Arduino_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.onButton = new System.Windows.Forms.Button();
+            this.offButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            // 
+            // onButton
+            // 
+            this.onButton.Location = new System.Drawing.Point(72, 127);
+            this.onButton.Name = "onButton";
+            this.onButton.Size = new System.Drawing.Size(75, 23);
+            this.onButton.TabIndex = 0;
+            this.onButton.Text = "Turn On";
+            this.onButton.UseVisualStyleBackColor = true;
+            this.onButton.Click += new System.EventHandler(this.onButton_Click);
+            // 
+            // offButton
+            // 
+            this.offButton.Location = new System.Drawing.Point(153, 127);
+            this.offButton.Name = "offButton";
+            this.offButton.Size = new System.Drawing.Size(75, 23);
+            this.offButton.TabIndex = 1;
+            this.offButton.Text = "Turn Off";
+            this.offButton.UseVisualStyleBackColor = true;
+            this.offButton.Click += new System.EventHandler(this.offButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 380);
+            this.Controls.Add(this.offButton);
+            this.Controls.Add(this.onButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button onButton;
+        private System.Windows.Forms.Button offButton;
+    }
+}
+
---
+++ STM32 GUI/Arduino GUI/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arduino_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+        }
+
+        private void onButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("A");
+        }
+
+        private void offButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("a");
+        }
+    }
+}
